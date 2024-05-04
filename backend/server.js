9c51ee4f85bb6def12a52a2e2b61d4b6dcdd1fbe
UjjwolMagar09 <np03cs4a220349@heraldcollege.edu.np>
2024-05-05 00:37:25 +0545

    error handling is complete in backend for 404 erros

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';  
 dotenv.config();
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+// import products from './data/products.js';
 import productsRoutes from './routes/productRoutes.js'
 const port = process.env.PORT || 5000;
 
@@ -14,5 +15,8 @@ app.get('/',(req,res)=>{
 });
 
 app.use('/api/products',productsRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port,()=>console.log(`Server running at port ${port}`));
