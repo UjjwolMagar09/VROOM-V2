baf63a49661f0c5b14e044e639dfe16cbe7f1d58
UjjwolMagar09 <np03cs4a220349@heraldcollege.edu.np>
2024-05-09 15:51:04 +0545

    Revert "edited and added Footer component design"
    This reverts commit a667f9ad980331c0a20f308e142c41f523e24d64.

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,48 +1,17 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookF, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
-import { faMapMarkerAlt, faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
-import { faDotCircle } from '@fortawesome/free-regular-svg-icons';
-import '../assets/styles/footer.css'; // Import your CSS file
-
+import {Container, Row, Col} from "react-bootstrap";
 const Footer = () => {
+  const currentYear = new Date().getFullYear()
   return (
-    <footer className="footer">
-      <div className="footer__left">
-        <a href="#" className="footer-link help-link">HELP</a>
-        <a href="#" className="footer-link contact-us-link">Contact Us</a>
-        <a href="#" className="footer-link about-us-link">About Us</a>
-        <a href="#" className="footer-link faqs-link">FAQs</a>
-        <a href="#" className="footer-link privacy-policy-link">Privacy Policy</a>
-        <a href="#" className="footer-link terms-and-conditions-link">Terms and Conditions</a>
-      </div>
-      <div className="footer__middle">
-        <p className="footer__follow">FOLLOW US</p>
-        <div className="social-icons">
-        <a href="#"><FontAwesomeIcon icon={faFacebookF} className="svg-inline--fa" /></a>
-<a href="#"><FontAwesomeIcon icon={faInstagram} className="svg-inline--fa" /></a>
-<a href="#"><FontAwesomeIcon icon={faTwitter} className="svg-inline--fa" /></a>
-        </div>
-      </div>
-      <div className="footer__right">
-        <h3 className="footer__getintouch">GET IN TOUCH</h3>
-        <div className="contact-details">
-        <div className="contact-item">
-  <FontAwesomeIcon icon={faMapMarkerAlt} className="svg-inline--fa" />
-  <span className="contact-info-placeholder">Naxal, Kathmandu</span>
-</div>
-<div className="contact-item">
-  <FontAwesomeIcon icon={faEnvelope} className="svg-inline--fa fa-envelope" />
-  <span className="contact-info-placeholder">VroomCarRental@gmail.com</span>
-</div>
-<div className="contact-item">
-  <FontAwesomeIcon icon={faPhone} className="svg-inline--fa fa-phone" />
-  <span className="contact-info-placeholder">9818681423</span>
-</div>
-        </div>
-      </div>
+    <footer>
+      <Container>
+        <Row>
+          <Col className='text-center py-3'></Col>
+          <p>VROOM &copy; {currentYear}</p>
+        </Row>
+      </Container>
     </footer>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
