a0aef0a236a90f4bb6e949cd93b26a95b0e3a0e4
UjjwolMagar09 <np03cs4a220349@heraldcollege.edu.np>
2024-05-13 01:33:45 +0545

    admin funcationality added to add new sample product on the database and edit the sample product

--- frontend/src/slices/productApiSlice.js
+++ frontend/src/slices/productApiSlice.js
@@ -7,6 +7,7 @@ export const productApiSlice = apiSlice.injectEndpoints({
       query: ()=>({
         url: PRODUCTS_URL,
       }),
+      providesTags: ['Product'],
       keepUnusedDataFor: 5,
     }),
     getProductDetails: builder.query({
@@ -14,8 +15,28 @@ export const productApiSlice = apiSlice.injectEndpoints({
         url:`${PRODUCTS_URL}/${productId}`,
       }),
       keepUnusedDataFor:5,
-    })
+    }),
+    createProduct: builder.mutation({
+      query: () => ({
+        url: `${PRODUCTS_URL}`,
+        method: 'POST',
+      }),
+      invalidatesTags: ['Product'],
+    }),
+    updateProduct: builder.mutation({
+      query: (data) => ({
+        url: `${PRODUCTS_URL}/${data.productId}`,
+        method: 'PUT',
+        body: data,
+      }),
+      invalidatesTags: ['Products'],
+    }),
   }), 
 })
 
-export const { useGetProductsQuery, useGetProductDetailsQuery } = productApiSlice;
+export const {
+  useGetProductsQuery,
+  useGetProductDetailsQuery,
+  useCreateProductMutation,
+  useUpdateProductMutation,
+} = productApiSlice;
